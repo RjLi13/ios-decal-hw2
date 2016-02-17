3b2eaeb32f44831c42ddd59b902be1253f40d706
RjLi13 <ruijing.li@berkeley.edu>
2016-02-17 00:12:34 -0800

    Finished

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -14,7 +14,10 @@ class KeyboardViewController: UIInputViewController {
     
     var keyboardView: UIView!
 
-
+    @IBOutlet weak var SecretLabel: UILabel!
+    
+    var awesomeBool: Bool = false
+    
     override func updateViewConstraints() {
         super.updateViewConstraints()
     
@@ -49,7 +52,7 @@ class KeyboardViewController: UIInputViewController {
  
     
     @IBAction func dankItUp(sender: UIButton){
-        let randInt = Int(arc4random_uniform(4))
+        let randInt = Int(arc4random_uniform(6))
         switch randInt{
         case 0:
             textDocumentProxy.insertText("much wow")
@@ -59,11 +62,59 @@ class KeyboardViewController: UIInputViewController {
             textDocumentProxy.insertText("420 blaze it")
         case 3:
             textDocumentProxy.insertText("EECS")
+        case 4:
+            textDocumentProxy.insertText("5/7")
+        case 5:
+            textDocumentProxy.insertText("2/10 would dank again")
+        default:
+            textDocumentProxy.insertText("wut duh")
+        }
+
+    }
+    
+    //could not figure out how to send images
+//    @IBAction func memeTastic(sender: UIButton){
+//        
+//    }
+    
+    @IBAction func foreignMemes(sender: UIButton){
+        let randInt = Int(arc4random_uniform(6))
+        switch randInt{
+        case 0:
+            textDocumentProxy.insertText("红老外")
+        case 1:
+            textDocumentProxy.insertText("차가와서")
+        case 2:
+            textDocumentProxy.insertText("4649")
+        case 3:
+            textDocumentProxy.insertText("un deux trois - quatre cinq")
+        case 4:
+            textDocumentProxy.insertText("Eso, si que es")
+        case 5:
+            textDocumentProxy.insertText("")
         default:
             textDocumentProxy.insertText("wut duh")
         }
         
     }
+    
+    
+    @IBAction func playVideo(sender: UIButton){
+       textDocumentProxy.insertText("https://www.youtube.com/watch?v=1ExuxDEXWgQ\n")
+        
+    }
+    
+    //For some reason does this not work and crashes my keyboard =(
+//    @IBAction func secretKey(sender: UIButton){
+//        if (!awesomeBool){
+//            SecretLabel.text = "OPEN THIS IN SAFARI"
+//            awesomeBool = true
+//        } else{
+//            textDocumentProxy.insertText("https://www.youtube.com/watch?v=1ExuxDEXWgQ\n")
+//            awesomeBool = false
+//            SecretLabel.text = "Secret"
+//        }
+//    }
     
     
     @IBAction func addSpace(sender: UIButton){
@@ -77,6 +128,7 @@ class KeyboardViewController: UIInputViewController {
         view.addSubview(keyboardView)
         view.backgroundColor = keyboardView.backgroundColor
         nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
+        keyboardView.backgroundColor = UIColor.brownColor()
     }
 
 
